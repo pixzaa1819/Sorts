a19a67477ab1bfb37e133bea6579b4046ba09c19
pixzaa1819 <132552941+pixzaa1819@users.noreply.github.com>
2023-05-06 20:41:07 +0800

    Add condition which nums could be null or length of 0

--- Sort.java
+++ Sort.java
@@ -1,35 +1,54 @@
 public class Sort {
     public static int[] bubbleSort(int[] nums) {
-        int n = nums.length;
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 0; i < n - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    // swap nums[i] and nums[i+1]
-                    int temp = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = temp;
-                    swapped = true;
-                }
+        if (nums==null)
+        {
+            return null;
+        }
+        else
+        {
+            int n = nums.length;
+            if (n>0)
+            {
+                boolean swapped;
+                do {
+                    swapped = false;
+                    for (int i = 0; i < n - 1; i++) {
+                        if (nums[i] > nums[i + 1]) {
+                            int temp = nums[i];
+                            nums[i] = nums[i + 1];
+                            nums[i + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+                    n--;
+                } while (swapped);
             }
-            n--;
-        } while (swapped);
-        return nums;
+            return nums;
+        }
     }
 
     public static int[] insertionSort(int[] nums) {
-        int n = nums.length;
-        for (int i = 1; i < n; ++i) {
-            int key = nums[i];
-            int j = i - 1;
-
-            while (j >= 0 && nums[j] > key) {
-                nums[j + 1] = nums[j];
-                j = j - 1;
-            }
-            nums[j + 1] = key;
+        if (nums==null)
+        {
+            return null;
         }
-        return nums;
+        else
+        {
+            int n = nums.length;
+            if (n>0)
+            {
+                for (int i = 1; i < n; ++i) {
+                    int key = nums[i];
+                    int j = i - 1;
+
+                    while (j >= 0 && nums[j] > key) {
+                        nums[j + 1] = nums[j];
+                        j = j - 1;
+                    }
+                    nums[j + 1] = key;
+                }
+            }
+            return nums;    
+        }
     }
 }
